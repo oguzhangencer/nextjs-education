66a05c68de46f41e3259e7e298e0e6e1f96b03f0
Oguzhan Gok <dev.oguzhangencer@gmail.com>
2024-04-28 15:54:14 +0300

    Update Home component to fetch posts from API instead of using local data

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,17 @@
 import styles from "./styles.module.css";
 import BlogCard from "@/components/BlogCard";
-import Data from "@/data/posts.json";
+// import Data from "@/data/posts.json";
+
+async function getPosts() {
+  const response = await fetch("https://dummyjson.com/posts?limit=10");
+  return response.json();
+}
 
 export default async function Home() {
+  const posts = await getPosts();
   return (
     <div className={styles.blogsContainer}>
-      {Data.posts.map((post) => (
+      {posts.posts.map((post) => (
         <BlogCard key={post.id} {...post} />
       ))}
     </div>
