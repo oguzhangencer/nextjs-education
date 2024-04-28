0a284e644ba6687cb166910f96cc1681eef7f9a9
Oguzhan Gok <dev.oguzhangencer@gmail.com>
2024-04-28 14:54:54 +0300

    Update Header component with logo and navigation links

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Link from "next/link";
+
+import styles from "./styles.module.css";
 
 function Header() {
-  return <header>Header</header>;
+  return (
+    <header className={styles.header}>
+      <Link href="/" className={styles.logo}>
+        <span>BLOG</span>
+      </Link>
+      <nav>
+        {/* target = "_blank" for opening the link in a new tab */}
+        <Link href="https://github.com/oguzhangencer" target="_blank">
+          About
+        </Link>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
