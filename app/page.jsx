5201e93c8bc80c191894a2bdab38975a5208c157
Oguzhan Gok <dev.oguzhangencer@gmail.com>
2024-04-28 15:08:54 +0300

    Update Home component with BlogCard import and layout changes

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,8 @@
-"use client";
+import BlogCard from "@/components/BlogCard";
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <BlogCard />
+    </div>
+  );
 }
