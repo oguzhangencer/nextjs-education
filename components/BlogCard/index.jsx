a4914c8c935d31d451831deee16633b68f008943
Oguzhan Gok <dev.oguzhangencer@gmail.com>
2024-04-28 15:20:01 +0300

    Update BlogCard component with image and layout changes

--- components/BlogCard/index.jsx
+++ components/BlogCard/index.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 
+import styles from "./styles.module.css";
+
 function BlogCard() {
   return (
-    <Link href="/">
-      <div>
-        <Image
-          src="https://picsum.photos/200/300"
-          alt="Blog Card Image"
-          width={96}
-          height={96}
-        />
+    <Link href="/" className={styles.card}>
+      <div className={styles.cardImage}>
+        <Image src="https://picsum.photos/200/300" alt="Blog Card Image" fill />
       </div>
-      <div>//title //description</div>
+      <div className={styles.cardBody}>
+        <h3>Title</h3>
+        <p>Lorem ipsum</p>
+      </div>
     </Link>
   );
 }
