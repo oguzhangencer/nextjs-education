5d870bfd0ff0e8b50beb08224d50f748d65612ed
Oguzhan Gok <dev.oguzhangencer@gmail.com>
2024-04-28 14:59:05 +0300

    Update Footer component with styles

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import styles from "./styles.module.css";
 
 function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className={styles.footer}>
+      <h4>Built with Next.js</h4>
+    </footer>
+  );
 }
 
 export default Footer;
