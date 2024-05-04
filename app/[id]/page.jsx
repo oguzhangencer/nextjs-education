c49e0e0060d3667b8944b45868a73516ea4142a6
Oguzhan Gok <dev.oguzhangencer@gmail.com>
2024-05-04 17:44:35 +0300

    Refactor page.jsx to fetch posts from API and update layout

---
+++ app/[id]/page.jsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import React from "react";
+import styles from "./styles.module.css";
+async function getPosts(id) {
+  const response = await fetch(`https://dummyjson.com/posts/${id}`);
+  return response.json();
+}
+
+export default async function Page({ params }) {
+  const { id, title, body } = await getPosts(params.id);
+
+  return (
+    <div className={styles.blogContainer}>
+      Blog
+      <div className={styles.cardImage}>
+        <Image
+          src={`https://picsum.photos/200/300?random=${id}`}
+          alt={title}
+          fill
+        />
+      </div>
+      <h1 className={styles.title}>{title}</h1>
+      <p>{body}</p>
+    </div>
+  );
+}
